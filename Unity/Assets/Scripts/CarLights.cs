77920b82ae071962878d1e59cade9984557db1c9
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-11-24 16:54:39 +0100

    Car lights script added; spotlight updated

---
+++ Unity/Assets/Scripts/CarLights.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarLights : MonoBehaviour {
+
+    // public Renderer ligths;
+    // public Material lightsON, lightsOFF;
+
+    public Light spotLightLeft, spotLightRight;
+    public bool lightsOn = true;
+
+    void turnLightsOn()
+    {
+        lightsOn = true;
+        spotLightLeft.intensity = 4f;
+        spotLightRight.intensity = 4f;
+    }
+
+    void turnLightsOff()
+    {
+        lightsOn = false;
+        spotLightLeft.intensity = 0;
+        spotLightRight.intensity = 0;
+    }
+
+    private void Start()
+    {
+        if (lightsOn)
+            turnLightsOff();
+        else
+            turnLightsOn();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown("1"))
+        {
+            if (lightsOn)
+                turnLightsOff();
+            else
+                turnLightsOn();
+        }
+    }
+}
