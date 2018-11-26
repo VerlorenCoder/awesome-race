d540bc4f5477a26ee7fdeff2a6c4c1309240c011
Grzegorz Oliwa <grzegorz.oliwa@outlook.com>
2018-11-26 19:52:49 +0100

    Deforming window on the back by clicking it

---
+++ Unity/Assets/Standard Assets/Vehicles/Car/Scripts/MeshDeformer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour {
+    Mesh deformingMesh;
+    Vector3[] originalVertices;
+
+    void Start() {
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = deformingMesh.vertices;
+    }
+
+    public void AddDeformingForce(Vector3 point) {
+        for (int i = 0; i < originalVertices.Length; i++) {
+            AddForceToVertex(i, point);
+        }
+    }
+
+    void AddForceToVertex(int i, Vector3 point) {
+        if(Random.Range(0, 30) == 0) {
+            Vector3 random = new Vector3(Random.Range(0, 0.003f), Random.Range(0, 0.001f), Random.Range(0, 0.003f));
+            originalVertices[i] = originalVertices[i] + random;
+        }
+    }
+
+    void Update() {
+        deformingMesh.vertices = originalVertices;
+        deformingMesh.RecalculateNormals();
+    }
+}
---
+++ Unity/Assets/Standard Assets/Vehicles/Car/Scripts/MeshDeformerInput.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MeshDeformerInput : MonoBehaviour {
+    public float force = 10f;
+    public float forceOffset = 0.1f;
+
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            HandleInput();
+        }
+    }
+
+    void HandleInput() {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(inputRay, out hit)) {
+            MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+
+            if (deformer) {
+                deformer.AddDeformingForce(hit.point);
+            }
+        }
+    }
+}
