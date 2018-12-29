54a453324e5aa45839ec7c1d8324526b1f1ca9ab
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-12-29 16:56:06 +0100

    Camera switching script added

---
+++ Unity/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public List<Camera> Cameras;
+    public int CurrentCameraIndex = 0;
+
+    private void Start()
+    {
+        Cameras[CurrentCameraIndex].depth = 999;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CurrentCameraIndex = (CurrentCameraIndex + 1) % Cameras.Count;
+
+            Cameras.ForEach(c =>
+            {
+                var index = Cameras.IndexOf(c);
+ 
+                if (index == CurrentCameraIndex)
+                {
+                    c.depth = 999;
+                } else
+                {
+                    c.depth = 0;
+                }
+            });
+        }
+    }
+}
