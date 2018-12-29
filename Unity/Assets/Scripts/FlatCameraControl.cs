eba0e5615c3fe1cce1564073aba6f4a4ea3d1c1d
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-12-29 17:15:22 +0100

    Initial FlatCamera and controlling script added

---
+++ Unity/Assets/Scripts/FlatCameraControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlatCameraControl : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetKey(KeyCode.N))
+        {
+            transform.localPosition += Vector3.down * 0.2f;
+        }
+        if (Input.GetKey(KeyCode.M))
+        {
+            transform.localPosition += Vector3.up * 0.2f;
+        }
+    }
+}
