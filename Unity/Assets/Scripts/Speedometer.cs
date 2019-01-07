7a4f6374eef7871032f0851897f8cc8a6b7c3b21
Kamil Bialucha <210347@student.pwr.edu.pl>
2019-01-07 08:23:33 +0100

    Basic speedometer added

---
+++ Unity/Assets/Scripts/Speedometer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public class Speedometer : MonoBehaviour {
+
+    private CarController Car;
+    public Text SpeedText;
+
+    private void Awake()
+    {
+        SpeedText.alignment = TextAnchor.MiddleRight;
+        Car = GetComponent<CarController>();
+    }
+
+    void Start () {
+        DisplayCurrentSpeed();
+    }
+
+    void Update () {
+        DisplayCurrentSpeed();
+	}
+
+    void DisplayCurrentSpeed()
+    {
+        var speed = (int) Mathf.Ceil(Car.CurrentSpeed);
+        var text = speed + " km/h";
+        SpeedText.text = text;
+    }
+}
