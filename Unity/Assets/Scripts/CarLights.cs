b28e192fb19dc7b3542dd2a605c11c5f920c93ca
Kamil Bialucha <210347@student.pwr.edu.pl>
2019-01-13 23:35:42 +0100

    Car brake lights handled

--- Unity/Assets/Scripts/CarLights.cs
+++ Unity/Assets/Scripts/CarLights.cs
@@ -2,44 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityStandardAssets.Vehicles.Car;
+
 public class CarLights : MonoBehaviour {
 
+    public CarController Car;
+
     // public Renderer ligths;
-    // public Material lightsON, lightsOFF;
+    // public Material brakeLightsON, brakeLightsOFF;
+    public Light spotLightLeft, spotLightRight, brakeLightLeft, brakeLightRight;
 
-    public Light spotLightLeft, spotLightRight;
     public bool lightsOn = true;
+    private bool brakeLightsOn = false;
 
-    void turnLightsOn()
+    public float brakeLightsBaseIntensity = 40f, brakeLightsIntensityMultiplier = 80f;
+
+    void turnHeadLightsOn()
     {
         lightsOn = true;
         spotLightLeft.intensity = 2f;
         spotLightRight.intensity = 2f;
     }
 
-    void turnLightsOff()
+    void turnHeadLightsOff()
     {
         lightsOn = false;
         spotLightLeft.intensity = 0;
         spotLightRight.intensity = 0;
     }
 
+    void turnBrakeLightsOn(float brakeInput)
+    {
+        brakeLightsOn = true;
+        brakeLightLeft.intensity = brakeLightsBaseIntensity + brakeLightsIntensityMultiplier * brakeInput;
+        brakeLightRight.intensity = brakeLightsBaseIntensity + brakeLightsIntensityMultiplier * brakeInput;
+
+        if (brakeInput == 0)
+        {
+            turnBrakeLightsOff();
+            brakeLightsOn = false;
+        }
+    }
+
+    void turnBrakeLightsOff()
+    {
+        brakeLightLeft.intensity = 0;
+        brakeLightRight.intensity = 0;
+    }
+
     private void Start()
     {
         if (lightsOn)
-            turnLightsOff();
+            turnHeadLightsOff();
         else
-            turnLightsOn();
+            turnHeadLightsOn();
     }
 
-    // Update is called once per frame
     void Update () {
 		if (Input.GetKeyDown("1"))
         {
             if (lightsOn)
-                turnLightsOff();
+                turnHeadLightsOff();
             else
-                turnLightsOn();
+                turnHeadLightsOn();
         }
+  
+        if (Car.BrakeInput > 0 || brakeLightsOn)
+            turnBrakeLightsOn(Car.BrakeInput);
     }
 }
