9935bc3469bb7c087a4f59533fe661e1bb375880
Grzegorz Oliwa <grzegorz.oliwa@outlook.com>
2019-01-16 16:08:39 +0100

    Smoke triggered after several crashes

--- Unity/Assets/Standard Assets/Vehicles/Car/Scripts/MeshDeformer.cs
+++ Unity/Assets/Standard Assets/Vehicles/Car/Scripts/MeshDeformer.cs
@@ -5,6 +5,10 @@ public class MeshDeformer : MonoBehaviour {
     Mesh deformingMesh;
     Vector3[] originalVertices;
     float strength = 0.01f;
+    private GameObject smoke;
+    private GameObject smokeBack;
+    private int crashes = 0;
+    private bool isOnFire = false;
 
     public void Crash() {
         AddDeformingForce();
@@ -13,12 +17,28 @@ public class MeshDeformer : MonoBehaviour {
     void Start() {
         deformingMesh = GetComponent<MeshFilter>().mesh;
         originalVertices = deformingMesh.vertices;
+        smoke = GameObject.Find("WhiteSmoke");
+        smoke.GetComponent<ParticleSystem>().enableEmission = false;
+        smokeBack = GameObject.Find("WhiteSmoke2");
+        smokeBack.GetComponent<ParticleSystem>().enableEmission = false;
     }
 
     public void AddDeformingForce() {
         Debug.Log("Bum!");
         for (int i = 0; i < originalVertices.Length; i++) {
             AddForceToVertex(i);
+        }
+
+        crashes++;
+
+        if(!isOnFire && crashes > 3) {
+            smoke.GetComponent<ParticleSystem>().enableEmission = true;
+            isOnFire = true;
+        }
+
+        if (isOnFire && crashes > 6)
+        {
+            smokeBack.GetComponent<ParticleSystem>().enableEmission = true;
         }
     }
 
