a8475cb4eae55a9aeb00552dbc26b678ee269a7c
Grzegorz Oliwa <grzegorz.oliwa@outlook.com>
2019-02-03 14:09:48 +0100

    Improved reset

--- Unity/Assets/Scripts/Car/MoveToStartPosition.cs
+++ Unity/Assets/Scripts/Car/MoveToStartPosition.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityStandardAssets.Vehicles.Car;
 
 public class MoveToStartPosition : MonoBehaviour
@@ -20,13 +21,7 @@ public class MoveToStartPosition : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Quaternion rotation = new Quaternion(0, 0, 0, 0);
-            Vector3 startPos = new Vector3(-7.39f, 1.95f, 4.6f);
-            GameObject.Find("Car").transform.position = startPos;
-            GameObject.Find("Car").transform.rotation = rotation;
-            timerText.color = Color.white;
-            timerText.text = "00:00.0";
-            GameObject.Find("Car").GetComponent<CarController>().RaceStop();
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
